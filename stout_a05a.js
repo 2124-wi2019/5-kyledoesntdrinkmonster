328e64af7f2c2b2249a3b1f8b0c5c2a57297f8dd
kyledoesntdrinkmonster <58920828+kyledoesntdrinkmonster@users.noreply.github.com>
2020-01-19 20:59:13 -0600

    Add files via upload

---
+++ stout_a05a.js
@@ -0,0 +1,29 @@
+/*Kyle Stout
+  stout_a05a
+  INFO 2124WW
+  Thoendel
+  1-19-2020
+*/
+
+let todolist = ["Eat", "Sleep", "Shower", "Brush Teeth"];
+todolist;
+todolist.unshift("Wake up");
+todolist;
+todolist.reverse();
+todolist.sort();
+todolist;
+todolist.includes("x");
+
+let grocerylist = new Array();
+grocerylist[0] = 'Milk';
+grocerylist[1] = 'Bread';
+grocerylist[2] = 'Sugar';
+grocerylist[3] = 'Flour';
+grocerylist[4] = 'Salt';
+grocerylist[5] = 'Cookies';
+grocerylist;
+grocerylist.shift();
+grocerylist.splice(4,0, 'Crackers');
+grocerylist.splice(4,0, 'Tea');
+grocerylist;
+
